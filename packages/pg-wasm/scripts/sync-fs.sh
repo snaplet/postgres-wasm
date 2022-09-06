8ee42b8c6b5e7f45eccc8f4305a3bcae1fbd8d02
Julien Goux <julien.goux@live.fr>
2022-09-06 21:37:40 +0200

    make sync-fs works

--- packages/pg-wasm/scripts/sync-fs.sh
+++ packages/pg-wasm/scripts/sync-fs.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
+# Get the current script directory
+script_dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+
+# Root of pg-wasm package
+root="$script_dir/.."
+
 # Clean up
-rm -rf filesystem
+rm -rf "$root/filesystem"
 
 # Copy the filesystem folder from the buildroot package
-cp -R ../buildroot/build/filesystem .
+cp -R "$root/../buildroot/build/filesystem" "$root"
 
 # Replace the bzimage url in the index.html based on the filesystem/filesystem.json file
-bzimage_bin_file=$(grep -oP '\"bzImage\"[,\d]+\"\K\w+\.bin' filesystem/filesystem.json)
-sed -i -E "s/const[[:space:]]+bzimageUrl[[:space:]]*=[[:space:]]*\"\.\/filesystem\/(.*)\"/const bzimageUrl = \"\.\/filesystem\/$bzimage_bin_file\"/" index.html
+bzimage_bin_file=$(grep -oP '\"bzImage\"[,\d]+\"\K\w+\.bin' "$root/filesystem/filesystem.json")
+sed -i -E "s/const[[:space:]]+bzimageUrl[[:space:]]*=[[:space:]]*\"\.\/filesystem\/(.*)\"/const bzimageUrl = \"\.\/filesystem\/$bzimage_bin_file\"/" "$root/index.html"
