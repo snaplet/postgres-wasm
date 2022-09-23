5307cbbfab03d41c71e5a52ac8567e7e5b31ca3b
Mark Burggraf <markb@mantisbible.com>
2022-09-23 06:20:18 -0700

    updated version

--- packages/websockproxy/publish.sh
+++ packages/websockproxy/publish.sh
@@ -1,2 +1,2 @@
-docker build --platform linux/amd64 -t burggraf/pg_browser_websockproxy:1.0.4 .
-docker push burggraf/pg_browser_websockproxy:1.0.4
+docker build --platform linux/amd64 -t burggraf/pg_browser_websockproxy:1.0.5 .
+docker push burggraf/pg_browser_websockproxy:1.0.5
