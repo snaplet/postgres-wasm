febe9aa92f21a4942a4f33e1ee521c71bc54b57a
Julien Goux <julien.goux@live.fr>
2022-09-27 09:45:09 +0200

    fix config mount and instructions

--- packages/buildroot/build.sh
+++ packages/buildroot/build.sh
@@ -4,7 +4,7 @@ docker run \
     --rm \
     -v $PWD/tools:/tools \
     -v $PWD/build:/build \
-    -v $PWD/config/config \
+    -v $PWD/config:/config \
     -ti \
     --platform linux/amd64 \
     buildroot
