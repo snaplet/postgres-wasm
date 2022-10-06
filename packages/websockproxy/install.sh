47f7559ad861d93a62c15d5035325ef2fdce3b74
Mark Burggraf <markb@mantisbible.com>
2022-10-06 07:09:20 -0700

    added -y to install scripts

--- packages/websockproxy/install.sh
+++ packages/websockproxy/install.sh
@@ -7,8 +7,8 @@ sleep 120 # give time for initial start
 
 crontab -l | { cat; echo "@reboot sleep 5 && docker rm -f relay && docker run --privileged --network host --name relay burggraf/pg_browser_websockproxy:1.0.5"; } | crontab -
 
-apt install certbot
-apt install cron
+apt install -y certbot
+apt install -y cron
 certbot certonly -d $PROXY_DOMAIN --standalone -n --agree-tos --email $PROXY_DOMAIN_SUPPORT_EMAIL
 docker cp -L /etc/letsencrypt/live/$PROXY_DOMAIN/fullchain.pem relay:/root/fullchain.pem
 docker cp -L /etc/letsencrypt/live/$PROXY_DOMAIN/privkey.pem relay:/root/privkey.pem
